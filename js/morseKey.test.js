f6fb39ca1f0935b553d447f8e83589cf58285b84
Behzad Sharafi <behzadsharafi@gmail.com>
2023-12-13 21:52:36 +1100

    updates the test

--- js/morseKey.test.js
+++ js/morseKey.test.js
@@ -3,7 +3,7 @@ import { objectReverse } from "./morseKey.js";
 describe("test cases for swapping the key value pairs of an object", () => {
   it("swaps the key and values in a given object correctly", () => {
     expect(objectReverse({ name: "Sam", nation: "USA" })).toEqual({
-      Sam: "name",
+      Sam: "nasme",
       USA: "nation",
     });
   });
